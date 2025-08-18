306f24e1f56b5c7296a2f42eb3181e039a07ea7c
Aaron Santiago <me@aaron.work>
2025-08-17 21:50:18 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
-import config from '3lib-config';
-config.init();
 
-console.log(config.get("hello", "sup"));
+
+export {
+  onBeforeInitialize,
+  onInitialize,
+  onAfterInitialize,
+
+  onBeforeUpdateEntry,
+  onUpdateEntry,
+  onAfterUpdateEntry,
+
+  onBeforeGetEntry,
+  onGetEntry,
+  onAfterGetEntry,
+}
+
+async function onBeforeInitialize(config, runApi) {
+  console.log('onBeforeInitialize', config["hello"]);
+}
+
+async function onInitialize(config, runApi) {
+  console.log('onInitialize');
+}
+
+async function onAfterInitialize(config, runApi) {
+  console.log("onAfterInitialize");
+}
+
+async function onBeforeUpdateEntry(config, runApi, id, data) {
+  console.log("onBeforeUpdateEntry");
+}
+
+async function onUpdateEntry(config, runApi, id, data) {
+  console.log("onUpdateEntry");
+}
+
+async function onAfterUpdateEntry(config, runApi, id, data) {
+  console.log("onAfterUpdateEntry");
+}
+
+async function onBeforeGetEntry(config, runApi, id) {
+  console.log("onBeforeGetEntry");
+}
+
+async function onGetEntry(config, runApi, id) {
+  console.log("onGetEntry");
+}
+
+async function onAfterGetEntry(config, runApi, id) {
+  console.log("onAfterGetEntry");
+}
